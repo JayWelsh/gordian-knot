99d541268560c0b3a8cb40f4ec0ebe9a7a1573ce
Jay Welsh <thealchemicalopus@gmail.com>
2021-09-24 15:09:30 +0200

    feat(contracts): initial contract version for dual-party payments

---
+++ contracts/OxCart.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract OxCart {
+  address gordianKnot;
+    
+  constructor (address _gordianKnot)  {
+    gordianKnot = _gordianKnot;
+  }
+  
+  fallback () external payable {
+    (bool deliverySuccess, ) = gordianKnot.call{value: address(this).balance}("");
+    require(deliverySuccess, "Ox cart delivery unsuccessful.");
+  }
+}
